313b226514a8c5faf90b5ba5ef00b998a2902c2e
Timofei <tim148@mail.ru>
2020-08-18 17:07:12 +0300

    Map Reduce

--- my_map.rb
+++ my_map.rb
@@ -15,19 +15,50 @@ class Array
       self.to_enum(:map)
     end
   end
-  def my_reduce
-    sum = 0
-    self.each do |n|
-      sum = yield(sum, n)
+  def my_reduce(sum = nil, symbol = nil)
+    if sum.nil? && symbol.nil?
+      sum = self.first
+      index = 0
+      self.each do |n|
+        if index != 0
+        sum = yield(sum, n)
+        end
+        index += 1
+      end
+    end
+    if sum.is_a?(Integer) && symbol.nil?
+      self.each do |n|
+        sum = yield(sum, n)
+      end
+    end
+    if symbol != nil
+      self.each do |n|
+        sum = n.send(symbol, sum)
+      end
+    end
+    if sum.is_a?(Symbol)
+      memo = self.first
+      index = 0
+      self.each do |n|
+        if index != 0
+        memo = memo.send(sum, n)
+        end
+        index += 1
+        end
+      sum = memo
     end
     sum
   end
 end
 array = [1, 2, 3, 4]
-# p array.my_map! {|n| n + 2}
-# p array.map {|n| n + 2}
-# p array.map(&:to_s)
-# p array.my_map(&:to_s)
-# p array
-puts array.my_reduce{|sum, n| sum * n}
-puts array.reduce{|sum, n| sum * n}
+p array.my_map! {|n| n + 2}
+p array.map {|n| n + 2}
+p array.map(&:to_s)
+p array.my_map(&:to_s)
+puts array.my_reduce(8){|sum, n| sum * n}
+puts array.reduce(8){|sum, n| sum * n}
+puts array.reduce(8, :+)
+puts array.my_reduce(8, :+)
+puts array.reduce(:*)
+puts array.my_reduce(:*)
+puts array.reduce
